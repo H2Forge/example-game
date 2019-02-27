7483233c5977e03b30079762bdcfd881afad3ca4
Martin <20317077@sun.ac.za>
2019-02-27 11:10:34 +0200

    Revert "Change it back?"
    This reverts commit e1c57ba804eb6604e4996073f657677b1ea342ec.

---
+++ How to make a Video Game Series/Assets/Scripts/Destructible.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+
+    public GameObject destroyedVersion;
+
+    public void Explode()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        GameObject.Find("Main Camera").GetComponent<CameraControl>().enabled = false;
+        Destroy(gameObject);
+    }
+}
